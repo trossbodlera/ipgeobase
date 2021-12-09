e6ceb284bce5d7ab83d7848150a60e9d34d0d432
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2021-12-09 17:44:00 +0800

    fix rubocup

--- Gemfile
+++ Gemfile
@@ -2,9 +2,9 @@
 
 source "https://rubygems.org"
 
+gem "minitest", "~> 5.0"
 gem "rake", "~> 13.0"
 gem "rubocop", "~> 1.7"
-gem "minitest", "~> 5.0"
 
 gem "addressable"
 gem "nokogiri-happymapper", require: "happymapper"
--- test/ipgeobase_test.rb
+++ test/ipgeobase_test.rb
@@ -4,19 +4,17 @@ require "test_helper"
 require_relative "../lib/ipgeobase"
 
 class IpgeobaseTest < Minitest::Test
-  def test_lookup
+  def setup
     fixture = File.read(File.expand_path("./fixtures/fixture.xml", __dir__))
-
     ip = "185.37.58.35"
-
     stub_request(:get, "http://ip-api.com/xml/#{ip}").to_return(status: 200, body: fixture)
+    @ip_meta = Ipgeobase.lookup(ip)
+  end
 
-    ip_meta = Ipgeobase.lookup(ip)
-
-    assert(ip_meta.city == "Irkutsk")
-    assert(ip_meta.country == "Russia")
-    assert(ip_meta.countryCode == "RU")
-    assert(ip_meta.lat * 10_000 == 522_978)
-    assert(ip_meta.lon * 10_000 == 1_042_964)
+  def test_lookup
+    assert(@ip_meta.city == "Irkutsk")
+    assert(@ip_meta.country == "Russia")
+    assert(@ip_meta.countryCode == "RU")
+    assert(@ip_meta.lat * 10_000 == 522_978)
   end
 end
